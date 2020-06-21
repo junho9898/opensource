c88b79d541b6637c180abc689b96c283eaa1adb8
ShiHyunKim <99amy614@naver.com>
2020-06-22 07:54:56 +0900

    Github

---
+++ polls/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from .models import Question, Choice
+
+# Register your models here.
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    extra = 10
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,              {'fields':['question_text']}),
+        ('Date information',{'fields':['pub_date']})
+    ]
+
+    list_display = ('question_text','pub_date','was_published_recently')
+
+    inlines = [ChoiceInLine]
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
+
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\99amy\desktop\sort1\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
